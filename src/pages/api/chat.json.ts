2a8d79e701bfd303085732f212b77fbfcf5fa321
Ayothegod <heyayomideadebisi@gmail.com>
2024-09-16 17:39:56 +0100

    add packages

--- src/pages/api/chat.json.ts
+++ src/pages/api/chat.json.ts
@@ -1,3 +1,37 @@
+import type { APIRoute } from "astro";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+export async function GET({}) {
+  return new Response(JSON.stringify({ msg: "Gemini pro builders" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
-// import.meta.env.NODE_ENV
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const genAI = new GoogleGenerativeAI(import.meta.env.NEW_GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const body = await request.json();
+
+    return new Response("response", {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error processing the AI response:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to process the AI response" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+};
